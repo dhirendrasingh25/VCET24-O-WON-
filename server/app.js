a0fec2f649ecca9f61843234cbc670ccbcfcd888
VivekZ79 <viveks03realm@gmail.com>
2024-10-04 14:14:05 +0530

    1 api

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import transactionRoutes from './routes/transactions.js'; // Correct the path as needed
 
 const app = express();
 
 const corsOptions = {}; // Define your CORS options here, if needed
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -15,8 +17,12 @@ app.use(cors(corsOptions));
 app.use(helmet());
 app.use(morgan('dev'));
 
+// Basic route for health check
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+// Transaction routes
+app.use('/api', transactionRoutes);
 
 export default app;
