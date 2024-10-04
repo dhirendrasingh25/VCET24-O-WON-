62c49031cfacd7fad4bb4cf532eb84f5c102b868
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2024-10-04 11:23:25 +0530

    Landing layout

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import LandingPage from './landing/page'
 import Navbar from '@/components/landingComponents/Navbar'
 import Footer from '@/components/landingComponents/Footer'
+import { Button } from '@/components/ui/button'
+import Quiz from '@/components/landingComponents/Quiz'
 
 const page = () => {
   return (
     <div className='h-full overflow-hidden'>
         <Navbar />
-        <LandingPage/>
+        <main className='h-full'>
+      <div id="home" className='h-screen'>
+        Home section for marketting
+      </div>
+      <div id="quiz" className='h-screen'>
+       <Quiz/>
+       <div className='flex items-center justify-center py-6'>
+        <Button>Join Us Today !</Button>
+       </div>
+      </div>
+      <div id="testimonial" className='h-screen'>
+        testimonial
+      </div>
+      <div id="about-us" className='h-screen'>
+        about us
+      </div>
+    </main>
         <Footer/>
     </div>
   )
