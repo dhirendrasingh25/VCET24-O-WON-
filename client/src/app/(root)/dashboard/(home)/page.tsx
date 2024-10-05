031970860924e89e9673a98a948e5e1e75c6ed07
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-10-05 12:13:07 +0530

    resolved async import

--- client/src/app/(root)/dashboard/(home)/page.tsx
+++ client/src/app/(root)/dashboard/(home)/page.tsx
@@ -45,6 +45,22 @@ export default function Dashboard() {
     const router = useRouter();
 
     useEffect(() => {
+        window.embeddedChatbotConfig = {
+            chatbotId: process.env.NEXT_PUBLIC_CHATBOT_ID as string,
+            domain: "www.chatbase.co",
+        };
+
+        const script = document.createElement("script");
+        script.src = "https://www.chatbase.co/embed.min.js";
+        script.defer = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    useEffect(() => {
         if (!session) return;
         async function checkProfile() {
             try {
--- client/src/app/(root)/dashboard/layout.tsx
+++ client/src/app/(root)/dashboard/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import SessionWrapper from "@/components/core/session-wrapper";
 import Sidebar from "@/components/dashboard/sidebar";
@@ -10,21 +8,7 @@ export default async function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    useEffect(() => {
-        window.embeddedChatbotConfig = {
-            chatbotId: process.env.NEXT_PUBLIC_CHATBOT_ID as string,
-            domain: "www.chatbase.co",
-        };
 
-        const script = document.createElement("script");
-        script.src = "https://www.chatbase.co/embed.min.js";
-        script.defer = true;
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
     return (
         <SessionWrapper>
             <main className="flex min-h-screen w-full flex-col bg-white/40">
